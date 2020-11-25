1a87548e3b2d8e558d3ed66ceb0faa954cd994d3
Georg Tuparev <gtupar@tuparev.com>
2020-11-25 16:05:39 +0200

    Initial usable version of POLIS provider related types

--- Sources/swift-polis/ServiceDiscovery/ServiceDiscovery.swift
+++ Sources/swift-polis/ServiceDiscovery/ServiceDiscovery.swift
@@ -5,18 +5,31 @@
 //  Created by Georg Tuparev on 18/11/2020.
 //
 
+/// This file defines types related to the POLIS provider discovery. The exact process is described in other documents.
+/// Later versions might implement a reference implementation of the discovery process, but every provider is free to
+/// implement its own procedures.
+///
+/// **Note for Swift developers:** Before releasing version 1.0 of this package all types will implement `Codable` to
+/// allow easy marshalling from JSON or XML data.
+
 import Foundation
 
-public struct PolisDirectory: Codable {
-    public let lastUpdate: Date
-    public var entries: [PolisDirectoryEntry]
-}
-
+/// POLIS APIs are either in XML or in JSON. For reasons stated elsewhere in the documentation XML APIs are preferred
+/// for production code. In contrast, JSON is often easier to be used for new development (no need of schema development)
+/// and often easier to be used from mobile and web applications. But because its fragility it should be avoided.
 public enum PolisDataFormatType: String, Codable {
     case xml
     case json
 }
 
+/// `PolisProviderType` defines different types of POLIS providers.
+///
+/// Only `public` provider should be used in production. Public providers should run on server with enough bandwidth and
+/// computational power capable of accommodating multiple parallel client requests every second. Only when a `public`
+/// server is unreachable, its `mirror` (if available) should be accessed until the main server is down.
+/// `private` provider's main purpose is to act as a local cache for larger institutions and should be not accessed from
+/// outside. They might require user authentication.
+/// `experimental` providers are sandboxes for new developments, and might require authentication.
 public enum PolisProviderType {
     case `public`         // default
     case `private`
@@ -24,9 +37,16 @@ public enum PolisProviderType {
     case experimental
 }
 
-public struct PolisDirectoryEntry: Codable {
+/// A list of known
+public struct PolisDirectory {
+    public let lastUpdate: Date
+    public var entries: [PolisDirectoryEntry]
+}
+
+/// All the information needed to identify a site as a POLIS provider
+public struct PolisDirectoryEntry {
     public let uid: String      // Globally unique ID (UUID version 4)
-    public let name: String
+    public let name: String     // Should be unique to avoid errors, but not a requirement
     public let domain: String   // Fully qualified, e.g. https://polis.observer
     public let description: String?
     public let lastUpdate: Date
@@ -34,9 +54,4 @@ public struct PolisDirectoryEntry: Codable {
     public let supportedAPIVersions: [String]
     public let supportedDataTypes: [PolisDataFormatType]
     public let providerType: PolisProviderType
-}
-
-public struct ObservingSiteReferenceDirectory {
-    public let lastUpdate: Date
-    public let sireReference: [ObservingSiteReference]
 }
