842ebf3d3539ffcfe438455e8b507d5bbbd392e9
Georg Tuparev <gtupar@tuparev.com>
2020-12-07 04:03:34 +0200

    Refining (mostly location) types

--- Sources/swift-polis/ServiceDiscovery/ObservingSite.swift
+++ Sources/swift-polis/ServiceDiscovery/ObservingSite.swift
@@ -24,17 +24,17 @@ public indirect enum SolarSystemBodyType {
     case Haumea
     case Makemake
     case Eris
-    case KuiperBeltObject(String)
+    case KuiperBeltObject(name: String)
 
-    case moon(SolarSystemBodyType, String)       // e.g. .moon(.Jupiter, "Titan")
-    case asteroid(String)
-    case comet(String)
+    case moon(planet: SolarSystemBodyType, name: String)       // e.g. .moon(.Jupiter, "Titan")
+    case asteroid(name: String)
+    case comet(name: String)
 }
 
 public enum ObservingSiteLocationType {
-    case earthGroundBasedFixed
-    case earthGroundBasedMobile
-    case earthAirBorn(altitudeInMeters: Double)                    // Altitude in km
+    case earthGroundBasedFixed(location: EarthLocation)
+    case earthGroundBasedMobile(description: String)
+    case earthAirBorn(range: AltitudeRange?)     // Altitude in km
     case groundBasedFixed(SolarSystemBodyType)
     case groundBasedMobile(SolarSystemBodyType)  // e.g. Mars rover
     case airBorn(SolarSystemBodyType, Double)
@@ -43,39 +43,55 @@ public enum ObservingSiteLocationType {
     case extraSolarSystem
 }
 
+public struct AltitudeRange {
+    let lowOrbit: Double  // m
+    let highOrbit: Double // m
+}
+
+public struct EarthLocation {
+    public let eastLongitude: Double  // degrees
+    public let latitude: Double       // degrees
+    public let altitude: Double?      // m
+}
+
 public struct ObservingSite {
-    // Location
+    public let location: ObservingSiteLocationType
     public let name: String
     public let startDate: Date? // Might be unknown
     public let endDate: Date?   // if != nil -> either closed or temporary created (e.g. solar eclipse monitoring)
     public let observatories: [Observatory]
+    public let description: String?
 }
 
 public enum ObservatoryType {
     // Location
-    case telescope(Telescope)
+    case opticalTelescope(OpticalTelescope)
     case radioAntenna(RadioAntenna)
 }
 
 public struct Observatory {
     let type: ObservatoryType
+    // Coordinates
 }
 
 public enum InstrumentType {
 }
 
-public struct Telescope {
+public struct OpticalTelescope {
     // Aperture
     // Camera
-    public let description: String?
+    // [Devices]
     // FocalLength
     // FocalRatio
-    // Mirrors
+    // [Mirrors]
+    // PlateScale
     // SpectralEfficiency
     // SpectralRegion
+    // Spectrograph
     // TrackRate
-    public let telescopes: [Telescope]?
     public let instruments: [InstrumentType]?
+    public let telescopes: [OpticalTelescope]?
+    public let description: String?
 }
 
 public struct RadioAntenna {
