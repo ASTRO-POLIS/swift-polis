c64cb6f2107828b4f9c33db5be03aa872510459c
Georg Tuparev <gtupar@tuparev.com>
2021-11-17 18:14:33 +0200

    Initial definitions of Observing Site Directory Entry.

--- Sources/swift-polis/Level_1_StatucDataSupport/PolisCommonStaticTypes.swift
+++ Sources/swift-polis/Level_1_StatucDataSupport/PolisCommonStaticTypes.swift
@@ -293,13 +293,35 @@ public struct PolisDirectory  {
     }
 }
 
-public struct ObservatorySiteDirectoryEntry: Codable {
-    public private(set) var attributes: PolisItemAttributes
+/// It is expected, that the list of observatory sites is long and each site's data could be way over 1MB. Therefore a
+/// compact list of site references is maintained separately containing only site UUIDs and last update time. It is
+/// recommended that clients cache this list and update the observatory data only in case the cache needs to be
+/// invalidated (e.g. lastUpdate is changed).
+///
+/// **Note for Swift developers:** COURAGEOUS and IMPORTANT ASSUMPTION: Types defined in this file and in
+/// `ServiceDiscovery.swift` should not have incompatible coding/decoding and API changes in future versions of the
+/// standard! All other types could evolve.
+
+/// This is only a quick reference to check if Client's cache has this site and if the site is up-to-date.
+public struct ObservingSiteReference: Codable, Identifiable {
+    public var attributes: PolisItemAttributes
+
+    public var id: UUID { attributes.id }
+
+    //TODO: Add site type and coordinates!
+    public init(attributes: PolisItemAttributes) {
+        self.attributes = attributes
+    }
 }
 
 public struct ObservatorySiteDirectory: Codable {
-    public var lastUpdate: Date                // Used for syncing
-    public var entries: [ObservatorySiteDirectoryEntry]
+    public var lastUpdate: Date                   // UTC
+    public var entries: [ObservingSiteReference]
+
+    public init(lastUpdate: Date, entries: [ObservingSiteReference]) {
+        self.lastUpdate = lastUpdate
+        self.entries    = entries
+    }
 }
 
 //MARK: - Making types Codable and CustomStringConvertible -
@@ -477,36 +499,5 @@ extension String {
 //TODO: Needs documentation!
 //TODO: Needs testing of all path methods
 
-
-/// It is expected, that the list of observatory sites is long and each site's data could be way over 1MB. Therefore a
-/// compact list of site references is maintained separately containing only site UUIDs and last update time. It is
-/// recommended that clients cache this list and update the observatory data only in case the cache needs to be
-/// invalidated (e.g. lastUpdate is changed).
-///
-/// **Note for Swift developers:** COURAGEOUS and IMPORTANT ASSUMPTION: Types defined in this file and in
-/// `ServiceDiscovery.swift` should not have incompatible coding/decoding and API changes in future versions of the
-/// standard! All other types could evolve.
-
-/// This is only a quick reference to check if Client's cache has this site and if the site is up-to-date.
-public struct ObservingSiteReference: Codable, Identifiable {
-    public var attributes: PolisItemAttributes
-
-    public var id: UUID { attributes.id }
-
-    //TODO: Add site type and coordinates!
-    public init(attributes: PolisItemAttributes) {
-        self.attributes = attributes
-    }
-}
-
-public struct ObservatorySiteDirectory: Codable {
-    public var lastUpdate: Date                   // UTC
-    public var entries: [ObservingSiteReference]
-
-    public init(lastUpdate: Date, entries: [ObservingSiteReference]) {
-        self.lastUpdate = lastUpdate
-        self.entries    = entries
-    }
-}
 
 
