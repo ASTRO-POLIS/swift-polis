8ad886e46f5cc1aebd642ecd5d87cc7752028881
Georg Tuparev <gtupar@tuparev.com>
2021-12-28 15:07:04 +0200

    Finalising PolisManufacturer

--- Tests/swift-polisTests/PolisCommonStaticTypesTests.swift
+++ Tests/swift-polisTests/PolisCommonStaticTypesTests.swift
@@ -57,6 +57,18 @@ final class PolisCommonStaticTypesTests: XCTestCase {
         XCTAssertNoThrow(try jsonDecoder.decode(PolisProviderType.self, from: string!.data(using: .utf8)!))
     }
 
+    func test_polisManufacturer() {
+        let sut_attributes = PolisItemAttributes(id: UUID(), status: PolisLifecycleStatus.inactive, lastUpdate: Date(), name: "Apple", automationLabel: "APPL")
+        let sut_admin      = PolisAdminContact(name: "Tim Cook", email: "tim@apple.com", additionalCommunicationChannels: [PolisCommunication](), notes: "The big boss")
+        let sut            = PolisManufacturer(attributes: sut_attributes, url: URL(string: "https://www.apple.com"), contact: sut_admin)
+
+        XCTAssertNotNil(sut)
+
+        data   = try? jsonEncoder.encode(sut)
+        string = String(data: data!, encoding: .utf8)
+        XCTAssertNoThrow(try jsonDecoder.decode(PolisManufacturer.self, from: string!.data(using: .utf8)!))
+}
+
     override func setUp() {
         super.setUp()
 
@@ -77,5 +89,6 @@ final class PolisCommonStaticTypesTests: XCTestCase {
         ("test_polisAttributesCodingAndDecoding",    test_polisAttributesCodingAndDecoding),
         ("test_polisProviderCodingAndDecoding",      test_polisProviderCodingAndDecoding),
         ("test_polisCommunicationCodingAndDecoding", test_polisCommunicationCodingAndDecoding),
+        ("test_polisManufacturer",                   test_polisManufacturer ),
     ]
 }
