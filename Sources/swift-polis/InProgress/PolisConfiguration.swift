5bcf34962e0d67bef2226f375238fddf4a738f09
Georg Tuparev <gtupar@tuparev.com>
2022-09-14 03:07:26 +0300

    Refining PolisConfiguration

--- Sources/swift-polis/InProgress/PolisConfiguration.swift
+++ Sources/swift-polis/InProgress/PolisConfiguration.swift
@@ -16,18 +16,40 @@
 
 import Foundation
 
-public enum PolisObservationResultType: String, Codable {
-    case simple2DImage
-    case multiFilter2DImage
-    case photometry
-    case spectrography
-
-    //TODO: Astronomers should define the entire list!
-}
 
 public struct PolisConfiguration: Codable {
-    public var observationResulType: PolisObservationResultType
+
+    public enum ObservationResultType: String, Codable {
+        case simple2DImage
+        case multiFilter2DImage
+        case photometry
+        case spectrography
+
+        //TODO: Astronomers should define the entire list!
+    }
+
+    public var observationResultType: ObservationResultType
     public var item: PolisItem
     public var deviceIDs: [UUID]
     public var isDefault = true
+}
+
+//MARK: - Making types Codable and CustomStringConvertible -
+
+public extension PolisConfiguration.ObservationResultType {
+    enum CodingKeys: String, CodingKey {
+        case simple2DImage      = "simple_2D_image"
+        case multiFilter2DImage = "multi_filter_2D_image"
+        case photometry
+        case spectrography
+    }
+}
+
+public extension PolisConfiguration {
+    enum CodingKeys: String, CodingKey {
+        case observationResultType = "observation_result_type"
+        case item
+        case deviceIDs             = "device_ids"
+        case isDefault             = "is_default"
+    }
 }
