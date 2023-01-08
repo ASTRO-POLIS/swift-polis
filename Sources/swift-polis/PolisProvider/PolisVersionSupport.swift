fa1927bd3cda226b4f1a8f8e856da452f0c02e77
Georg Tuparev <gtupar@tuparev.com>
2023-01-08 03:56:17 +0200

    Reformatting

--- Sources/swift-polis/PolisProvider/PolisVersionSupport.swift
+++ Sources/swift-polis/PolisProvider/PolisVersionSupport.swift
@@ -105,17 +105,127 @@ public struct PolisImplementationInfo: Codable, Equatable {
     }
 
     //MARK: - Private definitions -
-    private static var devicesSupportedByVersion = [SemanticVersion(with: "0.1-alpha.1") :
-                                                        [PolisDevice.DeviceType.telescope,
-                                                        ]
-    ]
+    private static var devicesSupportedByVersion = [SemanticVersion(with: "0.1-alpha.1") :[PolisDevice.DeviceType.telescope,]]
 
-    private static var subDevicesSupportedByVersion = [SemanticVersion(with: "0.1-alpha.1") :
-                                                        [PolisDevice.DeviceType.telescope :
-                                                            [PolisDevice.DeviceType.telescope,
-                                                            ],
-                                                        ]
-    ]
+    private static var subDevicesSupportedByVersion = [SemanticVersion(with: "0.1-alpha.1") : [PolisDevice.DeviceType.telescope :[PolisDevice.DeviceType.telescope,],]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
