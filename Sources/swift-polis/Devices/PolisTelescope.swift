a98d8a5dc463b70ac0bf94e15f0e1a6c4b1abea3
Thomas Jackson <tmjackson@tuparev.com>
2023-02-06 12:02:54 +0100

    minor change to configurations, addition of properties to PolisTelescope, documentation and editing.

--- Sources/swift-polis/Devices/PolisTelescope.swift
+++ Sources/swift-polis/Devices/PolisTelescope.swift
@@ -1,23 +1,101 @@
+//===----------------------------------------------------------------------===//
 //
-//  PolisTelescope.swift
-//  
+// This source file is part of the ASTRO-POLIS open source project
 //
-//  Created by Georg Tuparev on 3.02.23.
+// Copyright (c) 2021-2023 Tuparev Technologies and the ASTRO-POLIS project
+// authors.
+// Licensed under MIT License Modern Variant
 //
+// See LICENSE for license information
+// See CONTRIBUTORS.md for the list of ASTRO-POLIS project authors
+//
+// SPDX-License-Identifier: MIT-Modern-Variant
+//
+//===----------------------------------------------------------------------===//
 
 import Foundation
 
-// Notes / Questions:
-// - PolisTelescope does not need extra properties. All attributes should be defined here.
-// - Do we need a list of references back to possible Networks?
 
+/// `PolisTelescope` is an instrument to observe astronomical objects composed by a hierarchy of devices with the possiblity of being arranged in different configurations.
+///
 
-/// `PolisTelescope` is an  instrument to observe astronomical objects composed by a hierarchy of devices arranged in possibly different configurations.
-public struct PolisTelescope {
+public struct PolisTelescope: Codable {
+    
+    // defines the overall type of the telescope, i.e. how the combined devices function together as a system
+    public enum TelescopeType: String, Codable {
+        case reflector
+        case refractor
+        case steerableRadioDish
+        case staticRadioDish
+        case telephotoLens
+        case antenna
+        case other
+    }
+    
+    // defines the parts of the electromagnetic spectrum that the telescope is capable of observing
+    public enum ElectromagneticCoverage: String, Codable {
+        case gammaRay
+        case xRay
+        case ultraviolet
+        case optical
+        case infrared
+        case submillimetre
+        case millimetre
+        case radio
+    }
 
+    // item information as defined by POLIS Item
     public var item: PolisItem
-    public var deviceIDs: Set<UUID> // Only the root devices of the hierarchy
-    public var configurationIDs: Set<UUID>
+    
+    // type of telescope, i.e. how the combined devices function as an overall system
+    public var telescopeType: TelescopeType?
+    
+    // the parts of the electromagnetic spectrum that the telescope is capable of observing
+    public var emCoverage: [ElectromagneticCoverage]?
+    
+    // root devices of the hierarchy belonging to the telescope
+    public var deviceIDs: Set<UUID>
+    
+    // any POLIS configurations associated with the telescope
+    public var configurationIDs: Set<UUID>?
+    
+    // the parent device of the telescope (if applicable)
+    public var parentDevice: UUID?
+    
+    // the parent observatory of the telescope (if applicable)
+    public var parentObservatory: UUID?
+    
+    // networks that the telescope belongs to (VLBI or otherwise)
+    public var networks: [String]?
+    
+    // indication of if the telescope is capable of Interferometry or Very Long Baseline Interferometry (VLBI)
+    public var interferometerCapabilities: Bool?
+ 
+}
 
-    // We need some kind of reference to the enclosing observer site, but what about satellites and rovers?
+//MARK: - Making types Codable and CustomStringConvertible -
+
+public extension PolisTelescope.ElectromagneticCoverage {
+    enum CodingKeys: String, CodingKey {
+        case gammaRay         = "gamma_ray"
+        case xRay             = "x_ray"
+        case ultraviolet      = "ultraviolet"
+        case optical          = "optical"
+        case infrared         = "infrared"
+        case submillimeter    = "submillimeter"
+        case millimeter       = "millimeter"
+        case radio            = "radio"
+    }
+}
+
+public extension PolisTelescope.TelescopeType {
+    enum CodingKeys: String, CodingKey {
+        case reflector             = "reflector"
+        case refractor             = "refractor"
+        case steerableRadioDish    = "steerable_radio_dish"
+        case staticRadioDish       = "static_radio_dish"
+        case telephotoLens         = "telephoto_lens"
+        case antenna               = "antenna"
+        case other                 = "other"
+    }
 }
