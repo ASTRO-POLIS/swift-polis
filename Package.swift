6c9a843be672588e281957ba47a9e9f847097e3b
hasmik.mkhitaryan <mkhitaryanhasmik@gmail.com>
2023-03-06 15:51:03 +0400

    Change package ios version to 15.0

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "swift-polis",
     defaultLocalization: "en",
     platforms: [
-        .macOS(.v12), .iOS(.v14), .tvOS(.v11), .watchOS(.v5)
+        .macOS(.v12), .iOS(.v15), .tvOS(.v11), .watchOS(.v5)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
