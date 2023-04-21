bce40bbd5ec34e6d376d98066d72a1c28cc36bc2
Georg Tuparev <gtupar@tuparev.com>
2023-04-21 10:40:12 +0300

    Upgrading the Swift package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "swift-polis",
     defaultLocalization: "en",
     platforms: [
-        .macOS(.v12), .iOS(.v15), .tvOS(.v11), .watchOS(.v5)
+        .macOS(.v13), .iOS(.v15), .tvOS(.v11), .watchOS(.v5)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -16,8 +16,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "SoftwareEtudes", url: "https://github.com/tuparev/SoftwareEtudes", .branch("dev")),
-        .package(name: "ScienceEtudes",  url: "https://github.com/tuparev/ScienceEtudes",  .branch("dev"))
+        .package(url: "https://github.com/tuparev/SoftwareEtudes", branch: "dev"),
+        .package(url: "https://github.com/tuparev/ScienceEtudes",  branch: "dev"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
