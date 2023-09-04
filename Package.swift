25a7ba5737325e2ff21c6fd9def4e4f7656d9ad6
Georg Tuparev <gtupar@tuparev.com>
2023-09-04 13:07:12 +0300

    Incrementing os versions

--- Package.swift
+++ Package.swift
@@ -6,9 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-polis",
     defaultLocalization: "en",
-    platforms: [
-        .macOS(.v13), .iOS(.v15), .tvOS(.v11), .watchOS(.v5)
-    ],
+    platforms: [.macOS(.v13), .iOS(.v16), .tvOS(.v16), .watchOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "swift-polis", targets: ["swift-polis"]),
