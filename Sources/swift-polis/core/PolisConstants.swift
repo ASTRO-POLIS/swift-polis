92ddd1c3175ebc929ffd213f5352b8961723e503
Georg Tuparev <gtupar@tuparev.com>
2025-01-09 07:21:14 +0200

    Improving documentation

--- Sources/swift-polis/core/PolisConstants.swift
+++ Sources/swift-polis/core/PolisConstants.swift
@@ -4,7 +4,7 @@
 //
 // This source file is part of the ASTRO-POLIS open source project
 //
-// Copyright (c) 2021-2024 Tuparev Technologies and the ASTRO-POLIS project
+// Copyright (c) 2021-2025 Tuparev Technologies and the ASTRO-POLIS project
 // authors.
 // Licensed under MIT License Modern Variant
 //
@@ -18,35 +18,49 @@
 import Foundation
 import SoftwareEtudesUtilities
 
+/// As the name suggests, `PolisConstants` encapsulates global constants used by this framework.
+///
+/// **Note:** some of these constants are NOT part of the POLIS standard. They are used only within
+/// this framework.
 public struct PolisConstants {
 
-    /// This is the first and only POLIS service provider (and domain) that is (almost) guaranteed to be a valid public POLIS service provider.
+    /// This is the first and only POLIS service provider (and domain) that is (almost) guaranteed to be a valid
+    /// public POLIS service provider.
     ///
-    /// This constant defines the URL to the initial POLIS Service Provider. `testBigBangPolisDomain` is an experimental domain that is used to test new
-    /// POLIS development and is guaranteed to exit as well.
-    ///
-    /// Software packages trying for the first time to connect to a POLIS service provider should use these constants. In the future there might be (hopefully) many
-    /// more service providers, but an initial search is mostly guaranteed to be successful if `bigBangPolisDomain` is used.
+    /// Software packages trying for the first time to connect to a POLIS service provider should use this
+    /// URL. In the future there might be (hopefully) many more service providers, but an initial search
+    /// is mostly guaranteed to be successful if `bigBangPolisDomain` is used.
     public static let bigBangPolisDomain       = "https://polis.observer"
+
+    /// This constant defines the URL to the initial POLIS Test Service Provider.
+    ///
+    /// `testBigBangPolisDomain` is an experimental domain that is used to test new POLIS
+    /// features and is guaranteed to exit as well.
     public static let testBigBangPolisDomain   = "https://test.polis.observer"
 
     /// All global references should start with this string
+    ///
+    /// The meaning of *global* is defined elsewhere in the standard
     public static let polisReferencePrefix     = "ref://"
 
-    /// All UUIDs to local  container files should start with this string
+    /// All local references should start with this string
+    ///
+    /// The meaning of *local* is defined elsewhere in the standard
     public static let polisLocalPrefix         = "local://"
 
+    /// This file is not part of the POLIS standard, but it is needed if `swift-polis` is used to manage
+    /// the POLIS Provider. It is supposed to be in the folder that contains the `../polis/` root folder.
     public static let polisLocalConfigFileName = "polis_config.json"
+
     /// A list of supported implementations for this concrete framework.
     ///
-    /// Until we have a stable version there should be only one supported version. After version 1.0 of the standard is
-    /// released, we should start supporting past versions.
+    /// Until we have a stable version there should be only one supported version. After version 1.0 of the
+    /// standard is released, we should start supporting past versions.
     public static var frameworkSupportedImplementation: [PolisImplementation] =
     [
         PolisImplementation(dataFormat: PolisImplementation.DataFormat.json,
                             apiSupport: PolisImplementation.APILevel.staticData,
-                            version: SemanticVersion(with: "0.2.0-alpha.1")!
+                            version: SemanticVersion(with: "0.5.0-alpha.1")!
                            ),
     ]
-
 }
