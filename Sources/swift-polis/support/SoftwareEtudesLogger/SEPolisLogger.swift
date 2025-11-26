b668c6ebaf3bc88a17b76e4900edbab45e21be59
Ani Klekchyan <a.klekchyan@tuparev.com>
2025-11-26 09:34:35 +0400

    Implemented simple SEPolisLogger

--- Sources/swift-polis/support/SoftwareEtudesLogger/SEPolisLogger.swift
+++ Sources/swift-polis/support/SoftwareEtudesLogger/SEPolisLogger.swift
@@ -3,13 +3,22 @@
 //  swift-polis
 //
 //  Created by Ani Klekchyan on 22.11.25.
-//
 
 import Foundation
 import Logging
 import SoftwareEtudesLogging
-import SoftwareEtudesCoreMessageDispatching
 
-public final class SEPolisLogger {
+public enum SEPolisLogger {
     
+    /// Setup logging - call once at app start
+    public static func setup(subsystem: String = "com.polis.app", level: Logging.Logger.Level = .info) {
+        let dispatcher = OSLogDispatcher(subsystem: subsystem, category: "App")
+        let handler    = SoftwareEtudesLogging.Logger(logLevel: level, dispatchers: [dispatcher])
+        LoggingSystem.bootstrap { _ in handler }
+    }
+    
+    /// Get a logger
+    public static func logger(_ name: String = "default") -> Logging.Logger {
+        Logging.Logger(label: name)
+    }
 }
