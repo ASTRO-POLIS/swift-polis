7db45703e733b076d449e490ac93156e8f820fcb
Georg Tuparev <gtupar@tuparev.com>
2026-06-04 18:19:43 +0300

    Changing the way the Logger is configured

--- Sources/SwiftPolis/support/PolisLogger.swift
+++ Sources/SwiftPolis/support/PolisLogger.swift
@@ -10,6 +10,10 @@ import SoftwareEtudesLogging
 import SoftwareEtudesCoreMessageDispatching
 
 public enum PolisLogger {
+
+    ///  If the user of `PolisLogger` needs to set the login file path to another path, set `logFileURL` before calling the setup method
+    @MainActor public static var logFileURL: URL? = URL(string: "file:///tmp/polis.log")
+
     /// Setup logging - call once at app start
     /// - Parameters:
     ///   - subsystem: The subsystem identifier for OSLog (e.g., "com.polis.app")
@@ -17,9 +21,10 @@ public enum PolisLogger {
     ///   - logFileURL: Optional URL to a log file. If provided, logs will also be written to this file.
     ///   - includeConsole: If true, logs will also be printed to stdout (terminal). Default is false.
     ///   - includeOSLog: If true, logs will also be sent to OSLog (visible in Console.app). Default is false.
-    public static func setup(subsystem: String = "com.polis.package", level: Logging.Logger.Level = .info,
-                             logFileURL: URL? = nil, includeConsole: Bool = false, includeOSLog: Bool = false) {
+    @MainActor public static func setup(subsystem: String = "com.polis.package", level: Logging.Logger.Level = .info,
+                                        logFileURL: URL? = nil, includeConsole: Bool = false, includeOSLog: Bool = false) {
 
+        let actualLogFileURL: URL? = logFileURL == nil ? logFileURL : Self.logFileURL
         var dispatchers: [MessageDispatching] = []
 
         if includeOSLog {
@@ -29,17 +34,18 @@ public enum PolisLogger {
         if includeConsole {
             dispatchers.append(ConsoleDispatcher())
         }
-        
-        if let fileURL = logFileURL {
+
+        //TODO: This does not work when subfolders does not exist. Perhaps somewhere (SE?) we have a method like `createSubfoldersIfNeeded()` ... if not we should add it to SE and use it here.
+        if let fileURL = actualLogFileURL {
             if let fileDispatcher = try? FileDispatcher(fileURL: fileURL) {
                 dispatchers.append(fileDispatcher)
             }
         }
-        
+
         let handler = SoftwareEtudesLogging.Logger(logLevel: level, dispatchers: dispatchers)
         LoggingSystem.bootstrap { _ in handler }
     }
-    
+
     /// Get a logger
     public static func logger(_ name: String = "default") -> Logging.Logger {
         Logging.Logger(label: name)
@@ -50,7 +56,7 @@ public enum PolisLogger {
     public static func flush(timeout: TimeInterval = 5.0) {
         // Post notification - SoftwareEtudes Logger will catch and flush
         NotificationCenter.default.post(name: SoftwareEtudesLogging.Logger.willTerminateNotification, object: nil)
-        
+
         // Give async flush operations time to complete
         Thread.sleep(forTimeInterval: timeout)
     }
