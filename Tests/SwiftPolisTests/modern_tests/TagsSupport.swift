4105206bcb52a6b3f7231eb592c9632813b1f03f
Georg Tuparev <gtupar@tuparev.com>
2026-06-22 22:27:50 +0300

    Starting modernising the test suits and adding new Polis Direction tests

---
+++ Tests/SwiftPolisTests/modern_tests/TagsSupport.swift
@@ -0,0 +1,13 @@
+//
+//  TagsSupport.swift
+//  swift-polis
+//
+//  Created by Georg Tuparev on 22/06/2026.
+//
+
+import Testing
+
+extension Tag {
+    @Tag static var simpleAssignmentsAndCalculations: Self
+    @Tag static var jsonCodable: Self
+}
---
+++ Tests/SwiftPolisTests/modern_tests/static_data_types/PolisDirectionTests.swift
@@ -0,0 +1,103 @@
+//===----------------------------------------------------------------------===//
+//  PolisDirectionTests.swift
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the ASTRO-POLIS open source project
+//
+// Copyright (c) 2021-2026 Tuparev Technologies and the ASTRO-POLIS project
+// authors.
+// Licensed under MIT License Modern Variant
+//
+// See LICENSE for license information
+// See CONTRIBUTORS.md for the list of ASTRO-POLIS project authors
+//
+// SPDX-License-Identifier: MIT-Modern-Variant
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+import SoftwareEtudesUtilities
+
+@testable import SwiftPolis
+
+//MARK: Modern
+import Testing
+
+struct PolisDirectionTests {
+
+    @Test("Testing exact direction algorithms", .tags(.simpleAssignmentsAndCalculations))
+    func simpleExactDirections() {
+        let inRange   = PolisDirection(exactDirection: 17.3)
+        let negative  = PolisDirection(exactDirection: 135.0)
+        let tooLarge  = PolisDirection(exactDirection: 370.0)
+        let southWest = PolisDirection(roughDirection: .southWest)
+
+        #expect(inRange.direction()   == 17.3)
+        #expect(negative.direction()  == 135.0)
+        #expect(tooLarge.direction()  == 10.0)
+        #expect(southWest.direction() == 225.0)
+    }
+
+    @Test("Testing exact rough algorithms", .tags(.simpleAssignmentsAndCalculations))
+    func simpleRoughDirections() {
+        let zero           = PolisDirection(roughDirection: .north)
+        let ninety         = PolisDirection(roughDirection: .east)
+        let southEast      = PolisDirection(roughDirection: .southEast)
+        let south          = PolisDirection(roughDirection: .south)
+        let southSouthWest = PolisDirection(roughDirection: .southSouthWest)
+
+        #expect(zero.direction()           == 0.0)
+        #expect(ninety.direction()         == 90.0)
+        #expect(southEast.direction()      == 135.0)
+        #expect(south.direction()          == 180.0)
+        #expect(southSouthWest.direction() == 202.5)
+
+        #expect(zero.roughDirection?.rawValue == "N")
+        #expect(southSouthWest.roughDirection?.rawValue == "SSW")
+
+        #expect(southSouthWest.roughDirection?.abbreviation() == "S/SW")
+
+        //TODO: $$ZH - Implement nearestRoughDirection() tests
+    }
+
+    @Test("Testing JSON Codable", .tags(.jsonCodable))
+    func jsonCoding() throws {
+        var data: Data!
+        var string: String!
+        var expectedString: String!
+
+        let zero           = PolisDirection(exactDirection: 0.1)
+        let southSouthWest = PolisDirection(roughDirection: .southSouthWest)
+
+        
+        data = try jsonEncoder.encode(zero)
+        string  = String(data: data, encoding: .utf8)
+        expectedString = """
+{  
+   "exact_direction" : 0.1
+}
+"""
+        #expect(string.removeAllWhitespacesAndNewLines() == expectedString.removeAllWhitespacesAndNewLines())
+
+        data = try jsonEncoder.encode(southSouthWest)
+        string  = String(data: data, encoding: .utf8)
+        expectedString = """
+{  
+   "rough_direction" : "SSW"
+}
+"""
+        #expect(string.removeAllWhitespacesAndNewLines() == expectedString.removeAllWhitespacesAndNewLines())
+
+    }
+
+    private var jsonEncoder: PrettyJSONEncoder!
+    private var jsonDecoder: PrettyJSONDecoder!
+    private var data: Data!
+    private var string: String!
+
+    init() {
+        jsonEncoder = PrettyJSONEncoder()
+        jsonDecoder = PrettyJSONDecoder()
+    }
+}
+
